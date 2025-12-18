041ae8f25b0f5f90574a7c0530be84e64c1ff5cb
oforbiggys-creator <oforbiggys@gmail.com>
2025-12-18 06:10:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -75,3 +75,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+// THEME LOGIC
+const toggle = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light");
+}
+
+toggle?.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("light") ? "light" : "dark"
+  );
+});
